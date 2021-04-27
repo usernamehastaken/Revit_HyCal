cba41c91f26865380acf047577f6dfbe148ff135
xx c <cxx_pb@126.com>
2021-04-27 17:50:48 +0800

    20210427-1

---
+++ Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.ApplicationServices;
+using Revit_FunExtension;
+
+namespace Revit_HyCal
+{
+    [Transaction(TransactionMode.Manual)]
+    class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uIApplication = commandData.Application;
+            UIDocument uIDocument = commandData.Application.ActiveUIDocument;
+            Document document = uIDocument.Document;
+
+            ////IList<Reference> references = uIDocument.Selection.PickObjects(ObjectType.Element);
+            //Reference reference = uIDocument.Selection.PickObject(ObjectType.Element);
+            //Element element = document.GetElement(reference.ElementId);
+            //ParameterSet parameterSet = element.Parameters;
+            ////parameterSet.
+            //Parameter parameter= element.get_Parameter(BuiltInParameter.RBS_CALCULATED_SIZE);
+            //string str = uIApplication.Application.SharedParametersFilename;
+
+            //TaskDialog.Show("1", str);
+            //Transaction transaction = new Transaction(document);
+            //transaction.Start("asdf");
+            //document.Application.OpenSharedParameterFile().Groups.Create("asdf");
+            //transaction.Commit();
+
+            SetNewParameterToInstanceWall(uIApplication, document.Application.OpenSharedParameterFile());
+
+
+            return Result.Succeeded;
+        }
+
+        public static bool SetNewParameterToInstanceWall(UIApplication app, DefinitionFile myDefinitionFile)
+        {
+            // create a new group in the shared parameters file
+            DefinitionGroups myGroups = myDefinitionFile.Groups;
+            DefinitionGroup myGroup = myGroups.Create("nnf");
+
+            // create an instance definition in definition group MyParameters
+            ExternalDefinitionCreationOptions option = new ExternalDefinitionCreationOptions("Instance_ProductDate", ParameterType.Text);
+            // Don't let the user modify the value, only the API
+            option.UserModifiable = false;
+            // Set tooltip
+            option.Description = "Wall product date";
+            Definition myDefinition_ProductDate = myGroup.Definitions.Create(option);
+
+            // create a category set and insert category of wall to it
+            CategorySet myCategories = app.Application.Create.NewCategorySet();
+            // use BuiltInCategory to get category of wall
+            Category myCategory = Category.GetCategory(app.ActiveUIDocument.Document, BuiltInCategory.OST_Walls);
+
+
+            myCategories.Insert(myCategory);
+
+            //Create an instance of InstanceBinding
+            InstanceBinding instanceBinding = app.Application.Create.NewInstanceBinding(myCategories);
+
+            // Get the BingdingMap of current document.
+            BindingMap bindingMap = app.ActiveUIDocument.Document.ParameterBindings;
+
+            // Bind the definitions to the document
+            bool instanceBindOK = bindingMap.Insert(myDefinition_ProductDate,
+                                            instanceBinding, BuiltInParameterGroup.PG_TEXT);
+            return instanceBindOK;
+        }
+    }
+}
--- Operation.cs
+++ Operation.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB.Mechanical;
 
 namespace Revit_HyCal
 {
-    public class Class1
+    [Transaction(TransactionMode.Manual)]
+    public class Operation
     {
+        public static ConnectorSet GetConnectorSet(Document document, ElementId elementId)
+        {
+            //from the elementid to get all the connectors
+            Element element = document.GetElement(elementId);
+            //is Group
+            Group group = element as Group;
+            if (group is null)
+            {
+                //category only in ['duct','family instance']
+                Duct duct = element as Duct;
+                if (duct is null)
+                {
+                    FamilyInstance familyInstance = element as FamilyInstance;
+                    if (familyInstance is null)
+                    {
+                        throw new Exception("选择的图元未有连接键或非机械图元！");
+                    }
+                    return familyInstance.MEPModel.ConnectorManager.Connectors;
+                }
+                else
+                {
+                    return duct.ConnectorManager.Connectors;
+                }
+            }
+            else
+            {
+                IList<ElementId> elementIds = group.GetMemberIds();
+                IList<Connector> connectors = new List<Connector>();
+                foreach (var v in elementIds)
+                {
+                    foreach (Connector connector in GetConnectorSet(document, v))
+                    {
+                        connectors.Add(connector);
+                    }
+                }
+                return connectors as ConnectorSet;
+            }
+
+        }
     }
 }
