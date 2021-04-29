0689b1fc9e581cdd85f463c72fcf2823f5fd156b
xx c <cxx_pb@126.com>
2021-04-29 17:35:56 +0800

    20210429

--- Class1.cs
+++ Class1.cs
@@ -9,6 +9,7 @@ using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using Autodesk.Revit.ApplicationServices;
 using Revit_FunExtension;
+using Autodesk.Revit.DB.Mechanical;
 
 namespace Revit_HyCal
 {
@@ -17,53 +18,127 @@ namespace Revit_HyCal
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            ////test
+            //test(commandData, ref message, elements);
+            //return Result.Succeeded;
+            ////test
+
+
             UIApplication uIApplication = commandData.Application;
             UIDocument uIDocument = commandData.Application.ActiveUIDocument;
             Document document = uIDocument.Document;
             //Basic_Funs.SetProjectUnits(document, UnitType.UT_HVAC_Airflow, DisplayUnitType.DUT_CUBIC_METERS_PER_HOUR);
-            //==传统选取元素方法
-            List<ElementId> elementIds = new List<ElementId>();
+
+            //*************传统选取元素方法
+            IList<ElementId> elementIds = new List<ElementId>();
             try
             {
-                IList<Reference> references = uIDocument.Selection.PickObjects(ObjectType.Element);
-                TaskDialog.Show("Prompt", "Please Select the Origin of Selection Before!");
-                ElementId elementId_origin = uIDocument.Selection.PickObject(ObjectType.Element).ElementId;
-                foreach(Reference reff in references)
-                {
-                    elementIds.Add(reff.ElementId);
-                }
-                //TaskDialog.Show("1", elementIds.Count.ToString());
-                //如果有组元素，分解组元素并加入references
-                IList<ElementId> groupElementIds = new List<ElementId>();
-                foreach (ElementId id in elementIds)
-                {
-                    Group group = document.GetElement(id) as Group;
-                    if (group != null) { groupElementIds.Add(id); }
-                }
-                if (groupElementIds.Count > 0)
-                {
-                    elementIds = elementIds.Except(groupElementIds).ToList<ElementId>();
-                    //TaskDialog.Show("1", elementIds.Count.ToString());
-                    foreach(ElementId id in groupElementIds)
-                    {
-                        Group group = document.GetElement(id) as Group;
-                        elementIds = elementIds.Union(group.GetMemberIds()).ToList<ElementId>();//此处还有非实体groupelementid
-                    }
-                }
-
+                elementIds = Operation.SelectPipeline(uIDocument, document);
             }
-            catch
+            catch (Exception e)
             {
-                TaskDialog.Show("Prompt", "HVAC Hydraulic Calculation Quit!");
+                TaskDialog.Show("Prompt", e.Message);
                 return Result.Failed;
             }
-            uIDocument.Selection.SetElementIds(elementIds);
-            //TaskDialog.Show("1", elementIds.Count.ToString());
-            FilteredElementCollector filter = new FilteredElementCollector(document, elementIds);
-            TaskDialog.Show("1", filter.GetElementCount().ToString());
-            //==按顺序录入管道系统,不使用连接键，使用碰撞
+            if (elementIds.Count == 0)
+            {
+                TaskDialog.Show("Prompt", "HVAC Hydraulic Calculation App Quit!");
+                return Result.Failed;
+            }
+
+            //*****************按顺序录入管道系统,使用连接键，不使用碰撞
+            
+
+
+
+
+
+
+
+            ////建立碰撞过滤器
+            //ElementIntersectsElementFilter filterInsectEl = new ElementIntersectsElementFilter(document.GetElement(elementIds[elementIds.Count - 1]));
+            ////建立待过滤图元过滤器
+            //elementIds.Remove(elementIds[elementIds.Count - 1]);
+            //FilteredElementCollector filterids = new FilteredElementCollector(document, elementIds);
+            //while (elementIds.Count > 0)
+            //{
+            //    //如果碰撞，去除图元，未碰撞
+            //    int Count = filterids.WherePasses(filterInsectEl).GetElementCount();
+            //    show_dia(Count.ToString());
+            //    //if ()
+            //    //{
+
+            //    //}
+            //    ////未碰撞图元提示或根据距离选择
+            //    //else
+            //    //{
+
+            //    //}
+            //}
+
+
+
+
 
             return Result.Succeeded;
+        }
+
+
+        public static void show_dia(string str)
+        {
+            TaskDialog.Show("1", str);
+        }
+        public void test(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uIDocument = commandData.Application.ActiveUIDocument;
+            Document document = uIDocument.Document;
+            //FilteredElementCollector f1 = new FilteredElementCollector(document);
+            //ElementId elementId= uIDocument.Selection.PickObject(ObjectType.Element).ElementId;
+            //ElementIntersectsElementFilter f2 = new ElementIntersectsElementFilter(document.GetElement(elementId));
+            //FilteredElementCollector f3 = f1.WherePasses(f2);
+            //show_dia(f3.GetElementCount().ToString());
+            IList<Reference> lstReference = uIDocument.Selection.PickObjects(ObjectType.Element);
+
+            uIDocument.Selection.SetElementIds(new List<ElementId>());
+            show_dia("1");
+            uIDocument.Selection.PickObjects(ObjectType.Element, new MassSelectionFilter(), "asdf", lstReference);
+        }
+
+
+        public void GetElementAtConnector(Autodesk.Revit.DB.Connector connector)
+        {
+            MEPSystem mepSystem = connector.MEPSystem;
+            if (null != mepSystem)
+            {
+                string message = "Connector is owned by: " + connector.Owner.Name;
+
+                if (connector.IsConnected == true)
+                {
+                    ConnectorSet connectorSet = connector.AllRefs;
+                    ConnectorSetIterator csi = connectorSet.ForwardIterator();
+                    while (csi.MoveNext())
+                    {
+                        Connector connected = csi.Current as Connector;
+                        if (null != connected)
+                        {
+                            // look for physical connections
+                            if (connected.ConnectorType == ConnectorType.End ||
+                                connected.ConnectorType == ConnectorType.Curve ||
+                                connected.ConnectorType == ConnectorType.Physical)
+                            {
+                                message += "\nConnector is connected to: " + connected.Owner.ToString();
+                                message += "\nConnection type is: " + connected.ConnectorType;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    message += "\nConnector is not connected to anything.";
+                }
+
+                TaskDialog.Show("Revit", message);
+            }
         }
 
     }
--- Operation.cs
+++ Operation.cs
@@ -7,6 +7,7 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI.Selection;
 
 namespace Revit_HyCal
 {
@@ -51,6 +52,62 @@ namespace Revit_HyCal
                 return connectors as ConnectorSet;
             }
 
+        }
+        public static IList<ElementId> SelectPipeline(UIDocument uIDocument,Document document)
+        {
+            //!!!Causion last one is element_origin
+            List<ElementId> elementIds = new List<ElementId>();
+            try
+            {
+                //pickup all the elementids of the pipeline
+                IList<Reference> references = uIDocument.Selection.PickObjects(ObjectType.Element);
+                foreach (Reference reff in references)
+                {
+                    elementIds.Add(reff.ElementId);
+                }
+                TaskDialog.Show("Prompt", "Please Select the Origin of Selection Before!");
+                
+                //pick up the original elementid of the pipeline
+                ElementId elementId_origin = uIDocument.Selection.PickObject(ObjectType.Element).ElementId;
+
+                //if the elementids contain the group elements , 
+                //elementids kick off the group elements and union the groupelementids
+                IList<ElementId> groupElementIds = new List<ElementId>();
+                foreach (ElementId id in elementIds)
+                {
+                    Group group = document.GetElement(id) as Group;
+                    if (group != null) { groupElementIds.Add(id); }
+                }
+                if (groupElementIds.Count > 0)
+                {
+                    elementIds = elementIds.Except(groupElementIds).ToList<ElementId>();
+                    foreach (ElementId id in groupElementIds)
+                    {
+                        Group group = document.GetElement(id) as Group;
+                        elementIds = elementIds.Union(group.GetMemberIds()).ToList<ElementId>();//causion getmenberids contains elements of not solid elementid
+                    }
+                }
+                elementIds.Add(elementId_origin);
+                return elementIds;
+            }
+            catch
+            {
+                throw new Exception("HVAC Hydraulic Calculation App Quit");
+            }
+        }
+
+    }
+
+    public class MassSelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element element)
+        {
+            return true;
+        }
+
+        public bool AllowReference(Reference refer, XYZ point)
+        {
+            return false;
         }
     }
 }
